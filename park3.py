7d80b68039cf2a5e0682d2ac852928dccddd3672
Chintya-aa <tyachintyachin@gmail.com>
2025-10-15 15:35:38 +0700

    first commit

---
+++ park3.py
@@ -0,0 +1,14 @@
+#1
+def konversi_suhu(value, suhu):
+    if suhu == 'C':
+        #mengonfersi celsius ke farenheit
+        return (value * 9/5) + 32
+    elif suhu == 'F':
+        #mengofersi farenheit ke celsius
+        return (value - 32) *5/9
+    else:
+        raise ValueError("suhu tidak valid. gunakan" \
+                         "'C' untuk celsius atau 'F' untuk farenheit.")
+    print(konversi_suhu(200, 'C'))
+    print(konversi_suhu(38, 'F'))
+
